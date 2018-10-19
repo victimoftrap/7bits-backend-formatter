803c86f6b419e08c55a1ef476be825825130e92f
Alexander Golovanov <monsquizo@gmail.com>
2018-10-19 19:59:49 +0600

    New version of formatter

--- src/main/java/it/sevenbits/formatter/Formatter.java
+++ src/main/java/it/sevenbits/formatter/Formatter.java
@@ -1,46 +1,42 @@
 package it.sevenbits.formatter;
 
 public class Formatter {
-    private static final String EXTRA_SPACE = "    ";
-    private static final char LCBRACE = '{';
-    private static final char RCBRACE = '}';
-    private static final char SEMICOLON = ';';
-    private static final char COMMA = ',';
-    private static final char SPACE = ' ';
-    private static final char CARRIAGE_RETURN = '\n';
-    private int tabLevel;
-    private boolean newLine;
+    private final String EXTRA_SPACE = "    ";
+    private final char LCBRACE = '{';
+    private final char RCBRACE = '}';
+    private final char SEMICOLON = ';';
+    private final char COMMA = ',';
+    private final char SPACE = ' ';
+    private final char CARRIAGE_RETURN = '\n';
+    private int checkedIndex;
+    private int indentLevel;
 
-    private void removeSpaces(StringBuilder sb, int from) {
-        while (sb.charAt(from) == SPACE || sb.charAt(from) == CARRIAGE_RETURN) {
-            sb.deleteCharAt(from);
-            from--;
-        }
-    }
-
-    private int preventSpaces(String source, int from) {
-        while (source.charAt(from+1) == SPACE || source.charAt(from+1) == CARRIAGE_RETURN) {
-            from++;
-        }
-        return from;
-    }
-
-    private void addNewLine(StringBuilder sb) {
-        sb.append(CARRIAGE_RETURN);
-        addIndent(sb);
-    }
-
-    private void addNewLineAt(StringBuilder sb, int index) {
-        sb.insert(index, CARRIAGE_RETURN);
-        for (int i = 0; i < tabLevel; i++) {
-            sb.insert(index+1, EXTRA_SPACE);
-        }
-    }
-
-    private void addIndent(StringBuilder sb) {
-        for (int i = 0; i < tabLevel; i++) {
+    private void makeIndent(StringBuilder sb) {
+        for (int i = 0; i < indentLevel; i++) {
             sb.append(EXTRA_SPACE);
         }
+    }
+
+    private void makeIndentAt(int position, StringBuilder sb) {
+        for (int i = 0; i < indentLevel; i++) {
+            sb.insert(position, EXTRA_SPACE);
+        }
+    }
+
+    private void makeNewLineAt(int position, StringBuilder sb) {
+        sb.insert(position, CARRIAGE_RETURN);
+        makeIndentAt(position+1, sb);
+    }
+
+    private int removeSpacesFrom(int index, StringBuilder sb) {
+        int startIndex = index;
+        while (startIndex > checkedIndex && sb.charAt(startIndex) == SPACE || sb.charAt(startIndex) == CARRIAGE_RETURN) {
+            startIndex--;
+        }
+        if (startIndex != index) {
+            sb.delete(startIndex+1, index+1);
+        }
+        return startIndex+1;
     }
 
     public String format(String source) {
@@ -48,27 +44,16 @@ public class Formatter {
 
         for (int i = 0; i < source.length(); i++) {
             stringBuilder.append(source.charAt(i));
-            if (source.charAt(i) == LCBRACE) {
-                removeSpaces(stringBuilder, stringBuilder.lastIndexOf(String.valueOf(LCBRACE))-1);
-                stringBuilder.insert(stringBuilder.lastIndexOf(String.valueOf(LCBRACE)), SPACE);
-                tabLevel++;
-                newLine = true;
-                i = preventSpaces(source, i);
-            }
-            if (source.charAt(i) == RCBRACE) {
-                removeSpaces(stringBuilder, stringBuilder.length()-2);
-                tabLevel--;
-                addNewLineAt(stringBuilder, stringBuilder.length()-1);
-                newLine = true;
-            }
-            if (source.charAt(i) == SEMICOLON) {
-                newLine = true;
-                i = preventSpaces(source, i);
-            }
 
-            if (newLine) {
-                addNewLine(stringBuilder);
-                newLine = false;
+            if (source.charAt(i) == LCBRACE) {
+                indentLevel++;
+                int index = i;
+                if (i != 0) {
+                     index = removeSpacesFrom(stringBuilder.lastIndexOf(String.valueOf(LCBRACE)) - 1, stringBuilder);
+                }
+                stringBuilder.insert(index, SPACE);
+                makeNewLineAt(index+2, stringBuilder);
+                checkedIndex = stringBuilder.length();
             }
         }
         return stringBuilder.toString();
--- src/main/java/it/sevenbits/formatter/Main.java
+++ src/main/java/it/sevenbits/formatter/Main.java
@@ -3,6 +3,6 @@ package it.sevenbits.formatter;
 public class Main {
     public static void main(String[] args) {
         Formatter formatter = new Formatter();
-        System.out.println(formatter.format("aaa{          bb;  cc;dd; ee;  fff   {ggg {    }}}"));
+        System.out.println(formatter.format("{{{}}}"));
     }
 }
